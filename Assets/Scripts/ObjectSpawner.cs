8b1af8d761d167bb70fd5a45430bb68846bed5f4
reeeeesa <theresan2003@gmail.com>
2021-07-01 10:05:54 +1200

    Prefabs and spawners

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public List<GameObject> objectSpawnList = new List<GameObject>();
+    public float timer;
+    public GameObject jumpUpPrefab, duckDownPrefab, dodgeLeftPrefab, dodgeRightPrefab; //add other items here
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 3f;
+        objectSpawnList.Add(jumpUpPrefab); //0
+        objectSpawnList.Add(duckDownPrefab); //1
+        objectSpawnList.Add(dodgeRightPrefab); //2
+        objectSpawnList.Add(dodgeLeftPrefab); //3
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnObjects();
+    }
+
+
+
+    private void SpawnObjects()
+    {
+        timer = timer - Time.deltaTime;
+
+
+
+        if (timer < 0)
+        {
+            timer = 3;
+            int randomNum = Random.Range(0, objectSpawnList.Count);
+            Instantiate(objectSpawnList[randomNum], this.transform.position, this.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ObstacleMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+
+
+    private void Movement()
+    {
+        transform.position = transform.position + new Vector3(0, 0, -1 * speed * Time.deltaTime);
+
+
+
+        if (transform.position.z < -30f)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("NPC Destroyed");
+        }
+    }
+}
