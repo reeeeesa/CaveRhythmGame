1f8ca477f6c33372c59276a136668df74103f2c9
reeeeesa <theresan2003@gmail.com>
2021-07-01 11:30:18 +1200

    Tunnel Spawner in progress

---
+++ Assets/Scripts/TunnelSpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelSpawner : MonoBehaviour
+{
+    public float timer;
+    public GameObject tunnelPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
