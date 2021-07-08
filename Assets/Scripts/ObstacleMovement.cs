926c535f8009c649b71f09e4e5980fff4305a4e0
reeeeesa <theresan2003@gmail.com>
2021-07-09 11:30:19 +1200

    More audio stuff

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -7,21 +7,30 @@ using UnityEngine;
 public class ObstacleMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-
-
+    [SerializeField] private SongManager songManager;
+    private float BeatsShownInAdvance = 4;
+    private Vector3 SpawnPos =  new Vector3(0f, 0.18f, 25f);
+    private Vector3 RemovePos = new Vector3(0f, 0.18f, -20f);
+    private float beatOfThisNote, songPosInBeats;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 10;
+        songManager = FindObjectOfType<SongManager>();
+        beatOfThisNote = songManager.
     }
 
 
-
     // Update is called once per frame
+    //the update function of music notes
     void Update()
     {
-        Movement();
+        transform.position = Vector2.Lerp(
+            SpawnPos,
+            RemovePos,
+            (BeatsShownInAdvance - (beatOfThisNote - songPosInBeats)) / BeatsShownInAdvance
+        );
     }
 
 
